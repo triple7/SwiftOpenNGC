c3b4ed5ba380a8365794b4f89dca48e660d9b29d
Yuma Antoine Decaux <yuma@astreos.space>
2025-05-04 16:14:13 +1000

    Adding predicate functions for all and any set of conditions

--- Sources/SwiftOpenNGC/SwiftOpenNGC+API.swift
+++ Sources/SwiftOpenNGC/SwiftOpenNGC+API.swift
@@ -8,5 +8,23 @@
 import Foundation
 
 extension SwiftOpenNGC {
-    
+
+    func filterByAll(
+        _ items: [OpenNGCEntry],
+        by predicates: ((OpenNGCEntry) -> Bool)...
+    ) -> [OpenNGCEntry] {
+        return items.filter { item in
+            predicates.allSatisfy { $0(item) }
+        }
+    }
+
+    func filterByAny(
+        _ items: [OpenNGCEntry],
+        by predicates: ((OpenNGCEntry) -> Bool)...
+    ) -> [OpenNGCEntry] {
+        return items.filter { item in
+            predicates.contains{ $0(item) }
+        }
+    }
+
 }
